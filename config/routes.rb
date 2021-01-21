0fc5e5dbd10ff54361adf0c4a23311820d62ae76
Daniel Tobon <daniel18.tobon@gmail.com>
2021-01-21 15:00:47 -0500

    Edit option

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+
+
 Rails.application.routes.draw do
 root 'company#index'
   get 'company/index'
+
+
+  resources :company
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
