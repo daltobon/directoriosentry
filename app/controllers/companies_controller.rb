4b14c4a2ac73ba61d0280507c8c1268760f2edbb
Daniel Tobon <daniel18.tobon@gmail.com>
2021-01-21 19:37:21 -0500

    deleted

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,46 @@
 class CompaniesController < ApplicationController
+	before_action :find_company, only: [:show, :update, :edit, :destroy]
+  
+  def index
+  	@companies= Company.all
+  end
+
+  def show
+  end
+
+  def new
+  	@company = Company.new
+  end
+
+  def edit
+  end
+
+  def create
+  	@company = Company.create(company_params)
+  	  	if @company.save  
+  		redirect_to @company
+ 	else
+ 		render :new
+  	end
+  end
+
+  def update
+   		if @company.update(company_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
+  end
+
+  def destroy
+  end
+
+private
+	  def company_params
+			params.require(:company).permit(:name_company, :controller, :phone, :email, :quantityoffice)
+	  end
+  	  def find_company
+		@company = Company.find(params[:id])		
+	  end
+
 end
